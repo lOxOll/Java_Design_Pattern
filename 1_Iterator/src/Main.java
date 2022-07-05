52e002b0a094acb37b1b29edd64be98c64f658cf
lOxOll <48136243+lOxOll@users.noreply.github.com>
2022-07-05 23:35:55 +0900

    Update Main.java

--- 1_Iterator/src/Main.java
+++ 1_Iterator/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		BookShelf bookShelf = new BookShelf(3);
-		bookShelf.appendBook(new MyBook("言の葉の庭"));
-		bookShelf.appendBook(new MyBook("三人のおっさん"));
-		bookShelf.appendBook(new MyBook("鹿の王"));
+		bookShelf.appendBook(new MyBook("ハリー・ポッターと賢者の石"));
+		bookShelf.appendBook(new MyBook("容疑者Xの献身"));
+		bookShelf.appendBook(new MyBook("星の王子さま"));
 
 		for (MyBook myBook : bookShelf) {
 			System.out.println(myBook.getName());
